cd228b2d49affb411c75e407780cef4a42f4c78f
OneCallAway <1054590332@qq.com>
2019-04-03 20:30:44 +0800

    静态数组初始化

---
+++ Day05-code/src/Demo01/Demo02Array.java
@@ -0,0 +1,19 @@
+package Demo01;
+
+/**
+ * @program: basic-code
+ * @description: 静态初始化
+ * @author: ty
+ * @create: 2019-04-03 20:22
+ *
+ * 动态初始化（指定长度）：在创建数组的时候，直接指定数组当中的数据元素个数
+ * 静态初始化（指定内容）：在创建数组的时候，不直接指定数据个数多少，而是直接将具体的数据内容进行指定
+ *
+ * 静态初始化格式：
+ * 数据类型 [] 数组名称 = new 数据类型[] ｛元素1，元素2，.....｝
+ **/
+public class Demo02Array {
+    public static void main(String[] args) {
+
+    }
+}
