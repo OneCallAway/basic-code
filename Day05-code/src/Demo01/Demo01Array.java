046d360fc4a9bc822199a9ebdcefa66391934848
OneCallAway <1054590332@qq.com>
2019-04-03 19:42:59 +0800

    数组

--- Day05-code/src/Demo01/Demo01Array.java
+++ Day05-code/src/Demo01/Demo01Array.java
@@ -11,14 +11,34 @@ package Demo01;
  * 特点：
  * 1：数组是一种引用数据类型
  * 2：数组当中的多个数据，类型必须统一
+ * 3：数组的长度在程序运行期间不可改变
+ *
+ * 数组的初始化：在内存当中创建一个数组，并且向其中赋予一些默认值
+ *
+ * 二种初始化方式：
+ * 1：动态初始化（指定长度）
+ * 2：静态初始化（指定内容）
+ *
+ * 动态初始化数组的格式：
+ * 数据类型 [] 数组名称 = new 数据类型[数组长度]
+ *
+ * 解析含义：
+ * 左侧数据类型：也就是数组当中保存的数据，全都是统一的什么类型
+ * 左侧的中括号：代表是一个数组
+ * 左侧的数据名称：给数组取名字
+ * 右侧的new ：代表创建数组的动作
+ * 右侧数据类型：必须和左边的数据类型保持一致
+ * 右侧中括号的长度：也就是数组当中，到底可以保存多少个数据,是一个int数字
  **/
 public class Demo01Array {
     public static void main(String[] args) {
-        int score1 = 100;
-        int score2 = 99;
-        int score3 = 98;
-        System.out.println(score3);//98
-        score3 = 100;
-        System.out.println(score3);//100
+        //存放300个int数据
+        int [] arrayA = new int[300];
+
+        //创建一个数组，可以存放十个double类型数据
+        double [] arrayB = new double[10];
+
+        //创建一个数组，可以存放五个字符串
+        String [] arrayC = new String[5];
     }
 }
