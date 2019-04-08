6c4c50fcd94186d7f56f619ccded7f7a03ca5fb3
OneCallAway <1054590332@qq.com>
2019-04-08 14:04:44 +0800

    静态初始化

--- Day05-code/src/Demo01/Demo02Array.java
+++ Day05-code/src/Demo01/Demo02Array.java
@@ -11,9 +11,17 @@ package Demo01;
  *
  * 静态初始化格式：
  * 数据类型 [] 数组名称 = new 数据类型[] ｛元素1，元素2，.....｝
+ *
+ * 注意事项：
+ * 虽然静态初始化没有直接告诉长度，但是根据大括号里面的元素具体内容，也可以自动推算出长度
  **/
 public class Demo02Array {
     public static void main(String[] args) {
+        //创建一个数组，里面装的都是int数字，5，15，25
+        int [] arrayA = new int[]{5,15,25};
+
+        //创建一个数组，用来装字符串“hello，world，java”
+        String [] arrayB = new String[]{"hello","world","java"};
 
     }
 }
