7efaf8b27ea39579ef8f59f880e4a066f13b1478
OneCallAway <1054590332@qq.com>
2019-04-09 02:13:55 +0800

    静态数组的使用“”

---
+++ Day05-code/src/Demo01/Demo04ArrayUse.java
@@ -0,0 +1,34 @@
+package Demo01;
+
+/**
+ * @program: basic-code
+ * @description:
+ * @author: ty
+ * @create: 2019-04-03 20:35
+ *
+ * 直接打印数组名称，得到的是数组对应的：内存地址哈希值
+ * 二进制：01
+ * 十进制：0123456789
+ * 十六进制：0123456789abcdef
+ *
+ * 访问数组元素的格式：数组名称[索引值]
+ * 索引值：就是一个int数字，代表数组当中元素的编号
+ * [注意]索引值从0开始，一直到“数组的长度-1”位置
+ **/
+public class Demo04ArrayUse {
+    public static void main(String[] args) {
+        //静态初始化的省略格式
+        int [] array = {10,20,30};
+        System.out.println(array);//[I@4554617c   [标志是一个数组，I表示是int类型，4554617c十六进制地址
+
+        //直接打印数组当中的元素
+        System.out.println(array[0]);//10
+        System.out.println(array[1]);//20
+        System.out.println(array[2]);//30
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++");
+
+        //也可以将数组当中的某一个单个元素，赋值交给变量
+        int num = array[1];
+        System.out.println(num);
+    }
+}
