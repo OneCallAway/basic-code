89f291a2893cd31fa843d5a4eed4fbaddbf6fbd2
OneCallAway <1054590332@qq.com>
2019-05-16 14:23:45 +0800

    求数组中的最大值和最小

---
+++ Day05-code/src/Demo03/Demo05ArrayMax.java
@@ -0,0 +1,24 @@
+package Demo03;
+
+/**
+ * @program: basic-code
+ * @description: 求数组当中的最大值
+ * @author: ty
+ * @create: 2019-05-14 00:43
+ **/
+public class Demo05ArrayMax {
+    public static void main(String[] args) {
+        int[] array = {5,10,15,30,50};
+
+        int max = array[0];//作比较的值
+        for (int i = 1; i < array.length; i++) {
+            //如果当前元素比max值更大，替换max值
+            if(array[i] > max){
+                max = array[i];
+            }
+        }
+        //谁的值最大就存在max中
+        System.out.println(max);
+
+    }
+}
