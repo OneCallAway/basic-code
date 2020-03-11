455b5ccaf19e9d281a00edcd26c33b1b862f3fdb
OneCallAway <1054590332@qq.com>
2020-03-12 07:32:47 +0800

    Arrays常用方法

---
+++ Day08-code/src/Demo03/Demo04Static.java
@@ -0,0 +1,10 @@
+package Demo03;
+
+/**
+ * @program: basic-code
+ * @description:
+ * @author: ty
+ * @create: 2020-03-12 06:40
+ **/
+public class Demo04Static {
+}
---
+++ Day08-code/src/Demo04/Demo01Arrays.java
@@ -0,0 +1,10 @@
+package Demo04;
+
+/**
+ * @program: basic-code
+ * @description:
+ * @author: ty
+ * @create: 2020-03-12 07:03
+ **/
+public class Demo01Arrays {
+}
